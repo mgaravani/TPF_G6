b5f67292ad3fec29760b8b66475c17e86bca53ee
fabricecotti <fcecottibruno@itba.edu.ar>
2024-07-29 10:06:26 -0300

    testing de la libreria string.h

--- Backend/main.c
+++ Backend/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (void){
+    char word[] = "palabra";
     int var = 250;
     printf("%d \n", var);
+    printf("%d\n", (int)strlen(word));
     return 0;
 }

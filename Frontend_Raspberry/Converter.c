63a6f3d0870644f771fb09e5a11057865f36e6b4
EmanuelAlbornoz <ealbornoz@itba.edu.ar>
2024-08-17 00:35:09 -0300

    Mejora de la funcion Converter

--- Frontend_Raspberry/Converter.c
+++ Frontend_Raspberry/Converter.c
@@ -5,72 +5,66 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define THRESHOLD 100
 #define DISP_CANT_X_DOTS 16
 #define DISP_CANT_Y_DOTS 16
 
-int main()
-{
-
-    joy_init();                 // Inicializa el joystick
-    disp_init();                // Inicializa el display
-    disp_clear();               // Limpia todo el display
-    joyinfo_t info;
-    dcoord_t myPoint;
-    int matriz[DISP_CANT_Y_DOTS][DISP_CANT_X_DOTS];
-
-    // Inicializa la semilla del generador de números aleatorios
+// Función para generar la matriz con valores aleatorios de 0 y 1
+void generar_matriz(int matriz[DISP_CANT_Y_DOTS][DISP_CANT_X_DOTS]) {
     srand(time(NULL));
 
-    // Rellenar la matriz con 0 y 1 de forma aleatoria
     for (int i = 0; i < DISP_CANT_Y_DOTS; i++) {
         for (int j = 0; j < DISP_CANT_X_DOTS; j++) {
             matriz[i][j] = rand() % 2;
         }
     }
+}
 
-    // Imprimir la matriz en el formato solicitado
-    printf("int matriz[FILAS][COLUMNAS] = {\n");
-    for (int i = 0; i < DISP_CANT_Y_DOTS; i++) {
-        printf("    {");
-        for (int j = 0; j < DISP_CANT_X_DOTS; j++) {
-            printf("%d", matriz[i][j]);
-            if (j < DISP_CANT_X_DOTS - 1) {
-                printf(", ");
-            }
-        }
-        printf("}");
-        if (i < DISP_CANT_Y_DOTS - 1) {
-            printf(",");
-        }
-        printf("\n");
-    }
-    printf("};\n");
+// Función para leer la matriz y actualizar el display
+void mostrar_matriz(int matriz[DISP_CANT_Y_DOTS][DISP_CANT_X_DOTS]) {
+    dcoord_t myPoint;
 
-    // Recorrer la matriz y actualizar el display según el valor de cada elemento
     for (uint8_t i = 0; i < DISP_CANT_Y_DOTS; i++) {
         for (uint8_t j = 0; j < DISP_CANT_X_DOTS; j++) {
             myPoint.x = j;
             myPoint.y = i;
             if (matriz[i][j] == 1) {
-                disp_write(myPoint, D_ON);   // Prende el LED en la posición (i, j)
+                disp_write(myPoint, D_ON);
             } else {
-                disp_write(myPoint, D_OFF);  // Apaga el LED en la posición (i, j)
+                disp_write(myPoint, D_OFF);
             }
-            
-            disp_update();  // Actualiza el display
+            disp_update();
         }
     }
+}
 
-    do
-	{
-		info = joy_read();				//las guardo en info
-		
-	} while (info.sw == J_NOPRESS);	//mientras no se presione el switch
-    
-    
+int main() {
+    joy_init();
+    disp_init();
+    disp_clear();
+
+    int matriz[DISP_CANT_Y_DOTS][DISP_CANT_X_DOTS];
+    joyinfo_t info = {0,0,J_NOPRESS};
+    time_t start_time, current_time;
+
+    // Obtén el tiempo de inicio
+    start_time = time(NULL);
+
+    do {
+        info = joy_read();  // Lee el estado del joystick
+        if (info.sw != J_NOPRESS) {  // Si se detecta una pulsación
+            generar_matriz(matriz);  // Genera una nueva matriz
+            mostrar_matriz(matriz);  // Muestra la nueva matriz en el display
+        }
+
+        // Obtén el tiempo actual
+        current_time = time(NULL);
+
+    } while (difftime(current_time, start_time) < 20);  // Ejecuta hasta que hayan pasado 20 segundos
+
+    // Limpiar y finalizar
     disp_clear();
     disp_update();
+    printf("Han pasado 20 segundos. El programa se terminará.\n");
 
     return 0;
 }

ab692bc68438c81757fde6a0f050e177a6cea842
EmanuelAlbornoz <ealbornoz@itba.edu.ar>
2024-08-17 00:21:34 -0300

    Funcion Converter Creada

--- Frontend_Raspberry/Converter.c
+++ Frontend_Raspberry/Converter.c
@@ -2,50 +2,72 @@
 #include <unistd.h>
 #include "disdrv.h"
 #include "joydrv.h"
+#include <stdlib.h>
+#include <time.h>
+
 #define THRESHOLD 100
-#define FILAS 16
-#define COLUMNAS 16
+#define DISP_CANT_X_DOTS 16
+#define DISP_CANT_Y_DOTS 16
 
 int main()
 {
-    joy_init();										//inicializa el joystick
-	disp_init();									//inicializa el display
-	disp_clear();									//limpia todo el display
-    void disp_write(dcoord_t coord, dlevel_t val);
-    dcoord_t pos = {DISP_MAX_X>>1 , 0 };	//pos es la posición actual, empieza en el centro de la parte inferior
-	dcoord_t npos = pos;
-    joyinfo_t coord = {0,0,J_NOPRESS};
-    int matriz[FILAS][COLUMNAS];
+
+    joy_init();                 // Inicializa el joystick
+    disp_init();                // Inicializa el display
+    disp_clear();               // Limpia todo el display
+    joyinfo_t info;
+    dcoord_t myPoint;
+    int matriz[DISP_CANT_Y_DOTS][DISP_CANT_X_DOTS];
 
     // Inicializa la semilla del generador de números aleatorios
     srand(time(NULL));
 
     // Rellenar la matriz con 0 y 1 de forma aleatoria
-    for (int i = 0; i < FILAS; i++) {
-        for (int j = 0; j < COLUMNAS; j++) {
+    for (int i = 0; i < DISP_CANT_Y_DOTS; i++) {
+        for (int j = 0; j < DISP_CANT_X_DOTS; j++) {
             matriz[i][j] = rand() % 2;
         }
     }
 
     // Imprimir la matriz en el formato solicitado
     printf("int matriz[FILAS][COLUMNAS] = {\n");
-    for (int i = 0; i < FILAS; i++) {
+    for (int i = 0; i < DISP_CANT_Y_DOTS; i++) {
         printf("    {");
-        for (int j = 0; j < COLUMNAS; j++) {
+        for (int j = 0; j < DISP_CANT_X_DOTS; j++) {
             printf("%d", matriz[i][j]);
-            if (j < COLUMNAS - 1) {
+            if (j < DISP_CANT_X_DOTS - 1) {
                 printf(", ");
             }
         }
         printf("}");
-        if (i < FILAS - 1) {
+        if (i < DISP_CANT_Y_DOTS - 1) {
             printf(",");
         }
         printf("\n");
     }
     printf("};\n");
 
-    return 0;
+    // Recorrer la matriz y actualizar el display según el valor de cada elemento
+    for (uint8_t i = 0; i < DISP_CANT_Y_DOTS; i++) {
+        for (uint8_t j = 0; j < DISP_CANT_X_DOTS; j++) {
+            myPoint.x = j;
+            myPoint.y = i;
+            if (matriz[i][j] == 1) {
+                disp_write(myPoint, D_ON);   // Prende el LED en la posición (i, j)
+            } else {
+                disp_write(myPoint, D_OFF);  // Apaga el LED en la posición (i, j)
+            }
+            
+            disp_update();  // Actualiza el display
+        }
+    }
+
+    do
+	{
+		info = joy_read();				//las guardo en info
+		
+	} while (info.sw == J_NOPRESS);	//mientras no se presione el switch
+    
     
     disp_clear();
     disp_update();
--- Frontend_Raspberry/makefile_frontend_rasberry.mak
+++ Frontend_Raspberry/makefile_frontend_rasberry.mak
@@ -1,3 +1,6 @@
+all: frontend Converter
 
 frontend: Frontend.c joydrv.o joydrv.h disdrv.o disdrv.h
 	gcc Frontend.c joydrv.o disdrv.o -o Frontend -Wall
+Converter: Converter.c joydrv.o joydrv.h disdrv.o disdrv.h
+	gcc Converter.c joydrv.o disdrv.o -o Converter -Wall
